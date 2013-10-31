745bd67374b8289a323a876ec0b70c324fc3c772
Nils Rydh <nils.rydh@gmail.com>
2013-10-31 11:39:24 +0100

    missing a comma in defaults

--- nearestbar/config/defaults.js
+++ nearestbar/config/defaults.js
@@ -4,7 +4,7 @@ define([], function() {
     //and url parameters.
     var defaults = {
         "appid": "", //"f7af18c42f3740d5abfa241e07874563",
-        "webmap": "1379c278ea7f4745a129bed636dd4e21" //"4104323340c04b4c8e06c00d05cd775c",
+        "webmap": "1379c278ea7f4745a129bed636dd4e21", //"4104323340c04b4c8e06c00d05cd775c",
         //"webmap": "a30160193b1f4922be09d62940f65696",//"de5ae0c2040c49d38e9ea0637454ac73", // "0eece0d5de2140e9a44d8050f943fd18", "de5ae0c2040c49d38e9ea0637454ac73"
         "oauthappid": "bAkrQgFPquOr8OXa", //"AFTKRmv16wj14N3z",
         "clearHash": "false",
